6349871e64ca28837dcd6518b03ae490493936f3
sergeisidorov123 <seregasidorov2015@mail.ru>
2024-07-05 21:56:37 +0500

    dltvparse

--- dltvbottg.py
+++ dltvbottg.py
@@ -2,7 +2,6 @@ import telebot
 from games import live, upcoming, results, userans
 from datetime import datetime, timedelta
 
-bot = telebot.TeleBot('6481972129:AAFrjvY6nd4KM5vAT0RPSLq6uE1zlu9BaoY')
 bot = telebot.TeleBot('tgtoken')
 
 
